a012f694e4f31d8050ec03b8d84fcd6420ba395f
Andrei Kuznetsov <120333999+izuma18@users.noreply.github.com>
2023-01-05 19:42:55 +0100

    Add files via upload

---
+++ data_base/sqlite_db.py
@@ -0,0 +1,27 @@
+import sqlite3 as sq
+from create_bot import bot
+
+def sql_start():
+    global base, cur
+    base = sq.connect('skola_cool_db')
+    cur = base.cursor()
+    if base:
+        print('Data base connected OK!')
+    base.execute('CREATE TABLE IF NOT EXISTS price(img TEXT, name TEXT PRIMARY KEY, description TEXT, price TEXT)')
+    base.commit()
+
+async def sql_add_command(state):
+    async with state.proxy() as data:
+        cur.execute('INSERT INTO price VALUES (?,?,?,?)', tuple(data.values()))
+        base.commit()
+
+async def sql_read(message):
+    for ret in cur.execute('SELECT * FROM price').fetchall():
+        await bot.send_photo(message.from_user.id, ret[0], f'{ret[1]}\nОписание: {ret[2]}\nЦена {ret[-1]}')
+
+async def sql_read2():
+    return cur.execute('SELECT * FROM price').fetchall()
+
+async def sql_delete_command(data):
+    cur.execute('DELETE FROM price WHERE name == ?', (data,))
+    base.commit()
---
+++ handlers/client.py
@@ -0,0 +1,32 @@
+from aiogram import types, Dispatcher
+from create_bot import dp, bot
+from keyboards import kb_client
+from aiogram.types import ReplyKeyboardRemove
+from data_base import sqlite_db
+
+
+#@dp.message_handler(commands=['start'])
+async def command_start(message : types.Message):
+    try:
+        await bot.send_message(message.from_user.id, '...', reply_markup=kb_client)
+        await message.delete()
+    except:
+        await message.reply('Необходимо написать боту:\nhttps://t.me/Baltuszka_legnica_test_bot')
+
+#@dp.message_handler(commands=['Режим_работы'])
+async def skola_open_command(message : types.Message):
+    await bot.send_message(message.from_user.id, 'Пишите в любое время')
+
+#@dp.message_handler(commands=['Расположение'])
+async def skola_place_command(message : types.Message):
+    await bot.send_message(message.from_user.id, 'Wjazdowa 3/1A, 59-200 Legnica')
+
+#@dp.message_handler(commands=['Цены'])
+async def skola_price_command(message : types.Message):
+    await sqlite_db.sql_read(message)
+
+def register_handlers_client(dp : Dispatcher):
+    dp.register_message_handler(command_start, commands=['start'])
+    dp.register_message_handler(skola_open_command, commands=['Режим_работы'])
+    dp.register_message_handler(skola_place_command, commands=['Расположение'])
+    dp.register_message_handler(skola_price_command, commands=['Цены'])
