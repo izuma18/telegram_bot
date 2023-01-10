6a08be849a8a35c89908d78ea6192d81869bf204
bot_telegram <iandrei.kuz@gmail.com>
2023-01-10 18:18:54 +0100

    0.11

--- bot_telegram.py
+++ bot_telegram.py
@@ -1,4 +1,6 @@
+import os
 import config
+
 from aiogram import Dispatcher, Bot
 from aiogram.utils import executor
 from data_base import sqlite_db
@@ -9,7 +11,11 @@ dp = Dispatcher(bot)
 
 async def on_startup(_):
     sqlite_db.sql_start()
+    await bot.set_webhook(config.URL_APP)
 
+
+async def on_shutdown(dp):
+    await bot.delete_webhook()
 
 from handlers import client, admin, other
 
@@ -17,4 +23,11 @@ client.register_handlers_client(dp)
 admin.register_handlers_admin(dp)
 other.register_handlers_other(dp)
 
-executor.start_polling(dp, skip_updates=True)
+#executor.start_polling(dp, skip_updates=True)
+executor.start_webhook(
+    dispatcher=dp,
+    webhook_path='',
+    on_startup=on_startup,
+    skip_updates=True,
+    host='0.0.0.0',
+    port=int(os.environ.get('PORT', 5000)))
