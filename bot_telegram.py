cb5b928918e80ef80c0511e3c9a3e406070c62f4
bot_telegram <iandrei.kuz@gmail.com>
2023-01-10 18:34:00 +0100

    0.12

--- bot_telegram.py
+++ bot_telegram.py
@@ -9,8 +9,11 @@ bot = Bot(token=config.TOKEN)
 dp = Dispatcher(bot)
 
 
-async def on_startup(_):
+async def on_start(_):
     sqlite_db.sql_start()
+
+
+async def on_startup():
     await bot.set_webhook(config.URL_APP)
 
 
@@ -23,11 +26,11 @@ client.register_handlers_client(dp)
 admin.register_handlers_admin(dp)
 other.register_handlers_other(dp)
 
-#executor.start_polling(dp, skip_updates=True)
-executor.start_webhook(
+executor.start_polling(dp, skip_updates=True)
+'''executor.start_webhook(
     dispatcher=dp,
     webhook_path='',
     on_startup=on_startup,
     skip_updates=True,
     host='0.0.0.0',
-    port=int(os.environ.get('PORT', 5000)))
+    port=int(os.environ.get('PORT', 5000)))'''
